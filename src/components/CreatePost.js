0a2974c63cf6fb035e64e47deb83bff5fca80e60
Rahul Choudhary <choudharyrahul1711@gmail.com>
2022-08-24 22:18:59 +0530

    custom hooks in createpost

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,9 +1,11 @@
-import { useState } from "react";
+// import { useState } from "react";
+import { firestore } from "../firebase";
+import { useFromInput } from "./hooks";
 
 function CreatePost() {
-  const [title, setTitle] = useState();
-  const [subTitle, setSubTitle] = useState();
-  const [content, setContent] = useState();
+  const title = useFromInput("");
+  const subTitle = useFromInput("");
+  const content = useFromInput("");
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -11,6 +13,13 @@ function CreatePost() {
     console.log("title", title);
     console.log("subTitle", subTitle);
     console.log("content", content);
+
+    firestore.collection("posts").add({
+      title: title.value,
+      subTitle: subTitle.value,
+      content: content.value,
+      createdAt: new Date(),
+    });
   }
   return (
     <div className="create-post">
@@ -19,21 +28,15 @@ function CreatePost() {
       <form onSubmit={handleSubmit}>
         <div className="form-field">
           <label>Title</label>
-          <input value={title} onChange={(e) => setTitle(e.target.value)} />
+          <input {...title} />
         </div>
         <div className="form-field">
           <label>Sub-Title</label>
-          <input
-            value={subTitle}
-            onChange={(e) => setSubTitle(e.target.value)}
-          />
+          <input {...subTitle} />
         </div>
         <div className="form-field">
           <label>content</label>
-          <textarea
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          ></textarea>
+          <textarea {...content}></textarea>
         </div>
 
         <button className="create-post-btn">Create Post</button>
--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,29 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
+// import firebase from "firebase";
+// import "firebase/firestore";
 
-const firebaseConfig = {
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCy2LsgKazkunvYxq5T23gj5BOmjokTOd8",
+//   authDomain: "react-hooks-blog-f5251.firebaseapp.com",
+//   projectId: "react-hooks-blog-f5251",
+//   storageBucket: "react-hooks-blog-f5251.appspot.com",
+//   messagingSenderId: "806767761487",
+//   appId: "1:806767761487:web:21136291290a576abe723a",
+//   measurementId: "G-L5LRNP4YF4",
+// };
+
+// // Initialize Firebase
+// // const app = initializeApp(firebaseConfig);
+// // const analytics = getAnalytics(app);
+// const firestore = firebase.firestore();
+
+// export { firestore };
+
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+// import "firebase/compat/auth";
+
+// Your web app's Firebase configuration
+const config = {
   apiKey: "AIzaSyCy2LsgKazkunvYxq5T23gj5BOmjokTOd8",
   authDomain: "react-hooks-blog-f5251.firebaseapp.com",
   projectId: "react-hooks-blog-f5251",
@@ -11,8 +33,8 @@ const firebaseConfig = {
   measurementId: "G-L5LRNP4YF4",
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+firebase.initializeApp(config);
 
 export const firestore = firebase.firestore();
+
+export default firebase;
